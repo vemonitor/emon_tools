3d56fd139ef719710a10f7914b766c52d91f5401
mano8 <eli.serra173@gmail.com>
2025-01-18 22:52:40 +0100

    improve api_bulk_structure example file

--- examples/api_bulk_structure.py
+++ examples/api_bulk_structure.py
@@ -11,13 +11,20 @@ Send 5 * 60 dummy data points at 1s interval,
 to supervised inputs.
 """
 import os
+import sys
+import argparse
 import time
 import random
+from typing import Optional
+from dotenv import load_dotenv
+from emon_tools.__init__ import __version__
 from emon_tools.emon_api_core import EmonEngines
 from emon_tools.emon_api_core import EmonProcessList
 from emon_tools.api_utils import Utils as Ut
 from emon_tools.fina_utils import Utils as Fut
 from emon_tools.emonpy import EmonPy
+
+SEAPARATOR = '-' * 20
 
 
 def get_emon_structure():
@@ -76,14 +83,14 @@ class DataBulk:
         result = self.cli.create_structure(
             structure=structure
         )
-        print('-' * 10)
-        print('-' * 10)
+        print("Emoncms Inputs Feeds structure supervision example.")
+        print(SEAPARATOR)
         print(
             "Emoncms Input Feeds structure verrified in "
             f"{time.perf_counter() - start_time}s.\n"
             "Only add needed Inputs or feeds from structure data."
         )
-        print('-' * 10)
+        print(SEAPARATOR)
         print(f"Nb added inputs: {result['nb_added_inputs']}")
         print(
             f"Nb updated inputs: {result['nb_updated_inputs']}"
@@ -129,13 +136,12 @@ class DataBulk:
                 "res": response,
                 "perf": time.perf_counter() - start_perf})
             time.sleep(0.1)
-        print('-' * 10)
-        print('-' * 10)
+        print(SEAPARATOR * 2)
         print(
             "Writing Emoncms Input Bulk random dummy data points. \n"
             "In total 5 * 60s at 1s interval"
         )
-        print('-' * 10)
+        print(SEAPARATOR)
         start_date = Ut.get_string_datetime_from_timestamp(
             perf[0]['start_time'],
             timezone=None
@@ -144,16 +150,34 @@ class DataBulk:
             perf[-1]['end_time'],
             timezone=None
         )
-        print('-' * 10)
+        print(SEAPARATOR)
         print(
             "Look  in your Emoncms instance at: \n"
             f"{self.cli.url}/graph/{extended[0]['feeds'][0]['id']} \n"
             f"Dummy data was writed from {start_date} to {end_date}."
         )
-        print('-' * 10)
+        print(SEAPARATOR)
         print(perf)
-        print('-' * 10)
+        print(SEAPARATOR)
         return perf
+
+    def extended_inputs(self):
+        """
+        Get extended Inputs structure from Emoncms Api
+        """
+        inputs, feeds = self.cli.get_extended_inputs_by_filters(
+            input_filter={
+                "nodeid": ["emon_tools_ex1"],
+                "name": ["I1", "I2", "I3"]
+            },
+            feed_filter={
+                "name": ["F2A", "I1", "F3"],
+            }
+        )
+        print(SEAPARATOR)
+        print("Inputs: ", inputs)
+        print(SEAPARATOR)
+        print("Feeds: ", feeds)
 
     @staticmethod
     def generate_dummy_data(
@@ -178,12 +202,110 @@ class DataBulk:
         return data
 
 
+def parse_args(args):
+    """
+    Parsing function
+    :param args: arguments passed from the command line
+    :return: return parser
+    """
+    # create arguments
+    parser = argparse.ArgumentParser(
+        prog='emon-tools',
+        description='Example usage of emonpy module.'
+    )
+    parser.add_argument(
+        '--all',
+        help='Execute all actions.',
+        required=False,
+        action='store_true'
+    )
+    parser.add_argument(
+        '-s', '--src',
+        help='Search for particualar inputs feeds structure.',
+        required=False,
+        action='store_true'
+    )
+    parser.add_argument(
+        '-a', '--ast',
+        help='Supervise particualar inputs feeds structure.',
+        required=False,
+        action='store_true'
+    )
+    parser.add_argument(
+        '-p', '--post',
+        help='Post input bulk dummy data.',
+        required=False,
+        action='store_true'
+    )
+    parser.add_argument(
+        '-v', '--version',
+        action='version',
+        version='emon-tools ' + __version__
+    )
+
+    # parse arguments from script parameters
+    return parser.parse_args(args)
+
+
+def get_actions(parsed: Optional[argparse.Namespace]):
+    """
+    Parsing function
+    :param args: arguments passed from the command line
+    :return: return parser
+    """
+    result = {
+        'all': False,
+        'src': True,
+        'ast': False,
+        'post': False,
+    }
+    if parsed is not None:
+        result = {
+            'all': parsed.all,
+            'src': parsed.src,
+            'ast': parsed.ast,
+            'post': parsed.post,
+        }
+    return result
+
+
 if __name__ == "__main__":
+    options = None
+    if len(sys.argv) > 1:
+        options = parse_args(sys.argv[1:])
+    options = get_actions(options)
+    print(SEAPARATOR * 2)
+    print("emon-tools Example usage. \n")
+    print("(to execute you will need set up your .env file)")
+    print(SEAPARATOR * 2)
+
+    load_dotenv()
+    emon_url = os.getenv("EMONCMS_URL")
+    api_key = os.getenv("API_KEY")
     emonpy = DataBulk(
-        os.getenv('EMONCMS_URL'), os.getenv('API_KEY'))
-    emonpy.init_structure(
-        structure=get_emon_structure()
-    )
-    emonpy.dummy_bulk(
-        structure=get_emon_structure()
-    )
+        url=emon_url,
+        api_key=api_key)
+
+    print(
+        "Search for particualar inputs feeds structure (--src): ",
+        options.get('src'))
+    if options.get('all', False) or options.get('src', False):
+        emonpy.extended_inputs()
+
+    print('\n', SEAPARATOR)
+    print(
+        "Supervise particualar inputs feeds structure (--ast): ",
+        options.get('ast'))
+    if options.get('all', False) or options.get('ast', False):
+        emonpy.init_structure(
+            structure=get_emon_structure()
+        )
+
+    print('\n', SEAPARATOR)
+    print(
+        "Post input bulk dummy data (--post): ",
+        options.get('post'))
+    if options.get('all', False) or options.get('post', False):
+        emonpy.dummy_bulk(
+            structure=get_emon_structure()
+        )
