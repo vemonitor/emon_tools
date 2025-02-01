a1eb7bc597649dde50efd18beed31e42a21e0832
mano8 <eli.serra173@gmail.com>
2025-02-01 11:02:16 +0100

    feat: add FilesHelper utility for scanning files in a directory

---
+++ emon_tools/fastapi/utils/files.py
@@ -0,0 +1,16 @@
+"""Files Helper utils module"""
+from os import scandir
+from os.path import isdir, isfile
+
+
+class FilesHelper:
+    """Files Helper class"""
+    @staticmethod
+    def scan_dir(file_path):
+        """Scan files from path"""
+        result = []
+        if isdir(file_path):
+            for file_item in scandir(file_path):
+                if isfile(file_item):
+                    result.append(file_item.name)
+        return result
