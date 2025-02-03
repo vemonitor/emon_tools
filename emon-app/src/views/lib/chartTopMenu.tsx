f10527d81fa451c0ad6eaaba721531e257f7697d
mano8 <eli.serra173@gmail.com>
2025-02-03 03:11:49 +0100

    feat: implement navigation menu for zoom functionality and enhance chart options

--- emon-app/src/views/lib/chartTopMenu.tsx
+++ emon-app/src/views/lib/chartTopMenu.tsx
@@ -1,6 +1,5 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import {
   Select,
   SelectContent,
@@ -9,15 +8,176 @@ import {
   SelectLabel,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
+import { Separator } from '@/components/ui/separator';
 import { Toggle } from '@/components/ui/toggle';
 import { setZoom, useDataViewer } from '@/stores/dataViewerStore';
-import { useQueryClient } from '@tanstack/react-query';
 import clsx from 'clsx';
-import { ArrowLeftFromLine, ArrowRightFromLine, BetweenHorizontalEnd, BetweenHorizontalStart, RotateCw, ZoomIn, ZoomOut } from 'lucide-react';
-import { MouseEventHandler } from 'react';
+import { ArrowLeftFromLine, ArrowLeftToLine, ArrowRightFromLine, ArrowRightToLine, ChartSpline, Eye, RotateCw, SearchCode, Unplug, ZoomIn, ZoomOut } from 'lucide-react';
+
+type NavigationMenuProps = {
+  can_go_back: boolean;
+  handleGoBack: () => void;
+  can_go_after: boolean;
+  handleGoAfter: () => void;
+  can_zoom_in: boolean;
+  handleZoomIn: () => void;
+  can_zoom_out: boolean;
+  handleZoomOut: () => void
+  can_go_start: boolean;
+  handleGoStart: () => void
+  can_go_end: boolean;
+  handleGoEnd: () => void
+  classBody?: string;
+}
+
+export function NavigationMenu({
+  can_go_back,
+  handleGoBack,
+  can_go_after,
+  handleGoAfter,
+  can_zoom_in,
+  handleZoomIn,
+  can_zoom_out,
+  handleZoomOut,
+  can_go_start,
+  handleGoStart,
+  can_go_end,
+  handleGoEnd,
+  classBody
+}: NavigationMenuProps) {
+  
+  return (
+    <div className={clsx(
+      "flex flex-row items-center",
+      classBody
+      )}>
+      <Button
+        variant={'ghost'}
+      >
+        <RotateCw />
+      </Button>
+      <Button
+        onClick={()=>handleZoomIn()}
+        disabled={!can_zoom_in}
+        title='Zoom In'
+        variant={'ghost'}
+      >
+        <ZoomIn />
+      </Button>
+      <Button
+        onClick={()=>handleZoomOut()}
+        disabled={!can_zoom_out}
+        title='Zoom Out'
+        variant={'ghost'}
+      >
+        <ZoomOut />
+      </Button>
+      <Button
+        onClick={()=>handleGoStart()}
+        disabled={!can_go_start}
+        variant={'ghost'}
+      >
+        <ArrowLeftToLine />
+      </Button>
+      <Button
+        onClick={()=>handleGoBack()}
+        disabled={!can_go_back}
+        variant={'ghost'}
+      >
+        <ArrowLeftFromLine />
+      </Button>
+      <Button
+        onClick={()=>handleGoAfter()}
+        disabled={!can_go_after}
+        variant={'ghost'}
+      >
+        <ArrowRightFromLine />
+      </Button>
+      
+      <Button
+        onClick={()=>handleGoEnd()}
+        disabled={!can_go_end}
+        variant={'ghost'}
+      >
+        <ArrowRightToLine />
+      </Button>
+    </div>
+  )
+}
+
+type NavMenuProps = {
+  classBody?: string;
+}
+
+export function NavGraphMenu({
+  classBody
+}: NavMenuProps) {
+  const nav_graph = useDataViewer((state) => state.nav_graph)
+
+  const {
+    go_back,
+    go_after,
+    zoom_in,
+    zoom_out,
+    go_start,
+    go_end,
+  } = useDataViewer()
+
+  return (
+    <NavigationMenu
+        can_go_back={nav_graph.can_go_back}
+        handleGoBack={go_back}
+        can_go_after={nav_graph.can_go_after}
+        handleGoAfter={go_after}
+        can_zoom_in={nav_graph.can_zoom_in}
+        handleZoomIn={zoom_in}
+        can_zoom_out={nav_graph.can_zoom_out}
+        handleZoomOut={zoom_out}
+        can_go_start={nav_graph.can_go_start}
+        handleGoStart={go_start}
+        can_go_end={nav_graph.can_go_end}
+        handleGoEnd={go_end}
+        classBody={classBody}
+      />
+  )
+
+}
 
 
+export function NavViewMenu({
+  classBody
+}: NavMenuProps) {
+  const nav_view = useDataViewer((state) => state.nav_view)
+
+  const {
+    go_back,
+    go_after,
+    zoom_in,
+    zoom_out,
+    go_start,
+    go_end,
+  } = useDataViewer()
+
+  return (
+    <NavigationMenu
+        can_go_back={nav_view.can_go_back}
+        handleGoBack={go_back}
+        can_go_after={nav_view.can_go_after}
+        handleGoAfter={go_after}
+        can_zoom_in={nav_view.can_zoom_in}
+        handleZoomIn={zoom_in}
+        can_zoom_out={nav_view.can_zoom_out}
+        handleZoomOut={zoom_out}
+        can_go_start={nav_view.can_go_start}
+        handleGoStart={go_start}
+        can_go_end={nav_view.can_go_end}
+        handleGoEnd={go_end}
+        classBody={classBody}
+      />
+  )
+
+}
 
 type ChartTopMenuProps = {
   classBody?: string;
@@ -26,32 +186,9 @@ type ChartTopMenuProps = {
 export function ChartTopMenu({
   classBody
 }: ChartTopMenuProps) { //can_zoom_in
-  const queryClient = useQueryClient();
-  const go_back = useDataViewer((state) => state.go_back)
-  const go_after = useDataViewer((state) => state.go_after)
-  const zoom_in = useDataViewer((state) => state.zoom_in)
-  const zoom_out = useDataViewer((state) => state.zoom_out)
-  const can_zoom_in = useDataViewer((state) => state.can_zoom_in)
-  const can_zoom_out = useDataViewer((state) => state.can_zoom_out)
-  
-  const handleGoBefore = (e: MouseEventHandler<HTMLButtonElement>) => {
-    go_back()
-    //queryClient.invalidateQueries({ queryKey: ['emon_fina_datas'] })
-  }
+  const can_zoom_view = useDataViewer((state) => state.can_zoom_view)
+  const toggle_can_zoom_view = useDataViewer((state) => state.toggle_can_zoom_view)
 
-  const handleGoAfter = (e: MouseEventHandler<HTMLButtonElement>) => {
-    //e.preventDefault()
-    go_after()
-    //queryClient.invalidateQueries({ queryKey: ['emon_fina_datas'] })
-  }
-  const handleZoomIn = (e: MouseEventHandler<HTMLButtonElement>) => {
-    //queryClient.cancelQueries({ queryKey: ['emon_fina_datas'] })
-    zoom_in()  
-  }
-  const handleZoomOut = (e: MouseEventHandler<HTMLButtonElement>) => {
-    //queryClient.cancelQueries({ queryKey: ['emon_fina_datas'] })
-    zoom_out()
-  }
   return (
   <div
     className={clsx(
@@ -66,52 +203,15 @@ export function ChartTopMenu({
       )}
     >
       <div className="flex flex-row items-center">
-        <Button
-          variant={'ghost'}
-        >
-          <RotateCw />
-        </Button>
-        <Button
-          onClick={handleZoomIn}
-          disabled={!can_zoom_in}
-          title='Zoom In'
-          variant={'ghost'}
-        >
-          <ZoomIn />
-        </Button>
-        <Button
-          onClick={handleZoomOut}
-          disabled={!can_zoom_out}
-          title='Zoom Out'
-          variant={'ghost'}
-        >
-          <ZoomOut />
-        </Button>
-        <Button
-          onClick={handleGoBefore}
-          variant={'ghost'}
-        >
-          <ArrowLeftFromLine />
-        </Button>
-        <Button
-          onClick={handleGoAfter}
-          variant={'ghost'}
-        >
-          <ArrowRightFromLine />
-        </Button>
-        <Button
-          onClick={handleGoBefore}
-          variant={'ghost'}
-        >
-          <BetweenHorizontalStart />
-        </Button>
-        <Button
-          onClick={handleGoAfter}
-          variant={'ghost'}
-        >
-          <BetweenHorizontalEnd />
-        </Button>
-        
+        <Toggle
+          id='zoom_graph_status'
+          aria-label="Graph/View Zoom Status" 
+          defaultPressed={can_zoom_view}
+          onPressedChange={()=>toggle_can_zoom_view()}
+        >Nav : {can_zoom_view === true ? <Eye /> : <ChartSpline />}</Toggle>
+        <Separator orientation='vertical' decorative={true} className='w-1'></Separator>
+        <NavGraphMenu
+        />
       </div>
       <div className="">
         <GraphOptions />
@@ -129,6 +229,7 @@ export function ChartTopMenu({
 export function ZoomSelector({
   classBody
 }: ChartTopMenuProps) {
+  const interval = useDataViewer((state) => state.interval)
   const time_window = useDataViewer((state) => state.time_window)
   const set_time_window = useDataViewer((state) => state.set_time_window)
 
@@ -137,25 +238,36 @@ export function ZoomSelector({
   }
 
   return (
-    <Select
-      value={setZoom.get_zoom_by_window(time_window).time_window.toString()}
-      onValueChange={handleSelectChange}
-    >
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select zoom" />
-      </SelectTrigger>
-      <SelectContent className={classBody}>
-        <SelectGroup>
-          <SelectLabel>Zoom</SelectLabel>
-          {setZoom.get_static_zooms()
-            .filter(val => val.time_window >= 1800 && val.time_window <= 62899200)
-            .map((val, index) => {
-              return (<SelectItem key={index} value={val.time_window.toString()}>{val.label}</SelectItem>)
-            })
-          }
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <div className="flex flex-row items-center">
+      <Input
+        type="number"
+        readOnly={true}
+        value={interval}
+        maxLength={8}
+        size={8}
+        className='w-20'
+      >
+      </Input>
+      <Select
+        value={setZoom.get_zoom_by_window(time_window).time_window.toString()}
+        onValueChange={handleSelectChange}
+      >
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select zoom" />
+        </SelectTrigger>
+        <SelectContent className={classBody}>
+          <SelectGroup>
+            <SelectLabel>Zoom</SelectLabel>
+            {setZoom.get_static_zooms()
+              .filter(val => val.time_window >= 1800 && val.time_window <= 62899200)
+              .map((val, index) => {
+                return (<SelectItem key={index} value={val.time_window.toString()}>{val.label}</SelectItem>)
+              })
+            }
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
   )
 }
 
@@ -165,18 +277,18 @@ export function GraphOptions({
   const connect_nulls = useDataViewer((state) => state.connect_nulls)
   const toggle_connect_nulls = useDataViewer((state) => state.toggle_connect_nulls)
 
-  const handleConnectNulls = () => {
-    toggle_connect_nulls()
-  }
-
   return (
-    <div className="flex flex-row gap-2 items-center">
+    <div
+      className={clsx(
+        "flex flex-row gap-2 items-center",
+        classBody)}
+    >
       <Toggle
         id='show_missing_data'
         aria-label="Missing data"
         defaultPressed={connect_nulls}
-        onPressedChange={handleConnectNulls}
-      >Missing data</Toggle>
+        onPressedChange={()=>toggle_connect_nulls()}
+      ><Unplug />Missing data</Toggle>
     </div>
   )
 }
