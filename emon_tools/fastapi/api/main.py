460432d4eb5412b4c18f4db55393593e6b4edbe1
mano8 <eli.serra173@gmail.com>
2025-02-20 23:58:21 +0100

    feat: include item, login, user, and private routers in the main API router

--- emon_tools/fastapi/api/main.py
+++ emon_tools/fastapi/api/main.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
 
+from emon_tools.fastapi.api.routes import items
+from emon_tools.fastapi.api.routes import login
+from emon_tools.fastapi.api.routes import private
+from emon_tools.fastapi.api.routes import users
 from emon_tools.fastapi.api.routes import emon_fina
 from emon_tools.fastapi.core.config import settings
 
 api_router = APIRouter()
+api_router.include_router(login.router)
+api_router.include_router(users.router)
+api_router.include_router(items.router)
 api_router.include_router(emon_fina.router)
