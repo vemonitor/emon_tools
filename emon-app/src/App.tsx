a07f5f84f8e13f2849e0bdffbdde7eafb55d90fd
mano8 <eli.serra173@gmail.com>
2025-03-02 18:54:39 +0100

    feat: implement routing in App component to manage views dynamically

--- emon-app/src/App.tsx
+++ emon-app/src/App.tsx
@@ -1,13 +1,42 @@
-import Layout from '@/layout'
-import {DataViewer} from '@/views/dataViewer'
+import Layout from '@/layout';
+import { Route, Routes } from 'react-router';
+import { routes } from './routes/routes';
+import { createElement } from 'react';
 
 
 function App() {
+
   
   return (
-    
     <Layout>
-      <DataViewer />
+      <Routes>
+        {routes.map((route, i) => (
+          route.routes ? (
+            <Route
+              key={i}
+              path={route.path}
+            >
+              <Route
+                  index
+                  element={createElement(route.element)}
+                />
+              {route.routes.map((subroute, j) => (
+                <Route
+                  key={`${i}_${j}`}
+                  path={subroute.path}
+                  element={createElement(subroute.element)}
+                />
+              ))}
+            </Route>
+          ) : (
+            <Route
+              key={i}
+              path={route.path}
+              element={createElement(route.element)}
+            />
+          )
+        ))}
+      </Routes>
     </Layout>
   )
 }
