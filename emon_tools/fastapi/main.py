f10115fafeb4d241fc93849e001bed56a45213a6
mano8 <eli.serra173@gmail.com>
2025-03-02 18:44:44 +0100

    feat: update CORS origins to include additional local development URLs

--- emon_tools/fastapi/main.py
+++ emon_tools/fastapi/main.py
@@ -18,8 +18,12 @@ app = FastAPI(
 )
 
 origins = [
+    "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "http://localhost:5173"
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://127.0.0.1:5174",
+    "http://localhost:5174",
 ]
 
 app.add_middleware(
