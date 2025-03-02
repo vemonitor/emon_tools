2b7fe07baa8e0be0c980cfe8201c3845319d6fda
mano8 <eli.serra173@gmail.com>
2025-03-02 18:55:07 +0100

    feat: integrate authentication and routing in Layout component

--- emon-app/src/layout.tsx
+++ emon-app/src/layout.tsx
@@ -1,51 +1,39 @@
-import { AppSidebar } from "@/views/app-sidebar"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
+import { AppSidebar } from "@/routes/app-sidebar"
 import { Separator } from "@/components/ui/separator"
+import { BrowserRouter } from "react-router";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
-import { Toaster } from "./components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster"
+import { Breadcrumbs } from "@/routes/breadcrumbs";
+import AuthProvider from "./providers/auth-provider";
+
 
 export default function Layout(
   { children }: { children: React.ReactNode }
 ) {
   return (
-  <SidebarProvider>
-    <AppSidebar />
-    <SidebarInset>
-      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-        <div className="flex items-center gap-2 px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
-      </header>
-      <div className="min-h-[100vh] md:min-h-min px-4">
-        {children}
-      </div>
-      <Toaster />
-    </SidebarInset>
-  </SidebarProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+              <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1" />
+                <Separator orientation="vertical" className="mr-2 h-4" />
+                <Breadcrumbs />
+              </div>
+            </header>
+            <div className="min-h-[100vh] md:min-h-min px-4">
+              {children}
+            </div>
+            <Toaster />
+          </SidebarInset>
+        </SidebarProvider>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
