6906142f63a5963ecf3c83866b8df2e8c122f34d
mano8 <eli.serra173@gmail.com>
2025-03-02 18:38:56 +0100

    feat: update API router to include EmonHost and archive routes, removing items route

--- emon_tools/fastapi/api/main.py
+++ emon_tools/fastapi/api/main.py
@@ -1,14 +1,16 @@
 from fastapi import APIRouter
 
-from emon_tools.fastapi.api.routes import items
 from emon_tools.fastapi.api.routes import login
-from emon_tools.fastapi.api.routes import private
 from emon_tools.fastapi.api.routes import users
 from emon_tools.fastapi.api.routes import emon_fina
-from emon_tools.fastapi.core.config import settings
+from emon_tools.fastapi.api.routes import emon_hosts
+from emon_tools.fastapi.api.routes import archive_group
+from emon_tools.fastapi.api.routes import archive_file
 
 api_router = APIRouter()
 api_router.include_router(login.router)
 api_router.include_router(users.router)
-api_router.include_router(items.router)
+api_router.include_router(emon_hosts.router)
+api_router.include_router(archive_group.router)
+api_router.include_router(archive_file.router)
 api_router.include_router(emon_fina.router)
