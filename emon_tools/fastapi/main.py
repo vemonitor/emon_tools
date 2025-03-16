04d8039ee38dc7446160fda98d96a64d11d25688
mano8 <eli.serra173@gmail.com>
2025-03-16 14:38:55 +0100

    feat: add static file serving with FastAPI and update unique ID generation for API routes

--- emon_tools/fastapi/main.py
+++ emon_tools/fastapi/main.py
@@ -3,11 +3,24 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from emon_tools.fastapi.api.main import api_router
 from emon_tools.fastapi.core.config import settings
 
 
 def custom_generate_unique_id(route: APIRoute) -> str:
+    """
+    Generate a unique identifier for a given API route.
+
+    Args:
+        route (APIRoute):
+            The API route for which to generate the unique identifier.
+
+    Returns:
+        str:
+            A unique identifier string composed of the route's
+            first tag and name.
+    """
     return f"{route.tags[0]}-{route.name}"
 
 
@@ -32,6 +45,12 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
+)
+
+app.mount(
+    '/static',
+    StaticFiles(directory=settings.STATIC_BASE_PATH),
+    name="static"
 )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
