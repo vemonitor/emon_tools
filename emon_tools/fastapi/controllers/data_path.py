58bca6032cbd55a79b39cb98404d4d3e084f1ac7
mano8 <eli.serra173@gmail.com>
2025-03-16 18:51:30 +0100

    feat: add DataPathController for retrieving and counting data paths

---
+++ emon_tools/fastapi/controllers/data_path.py
@@ -0,0 +1,50 @@
+"""
+DataPath Controller
+"""
+from sqlmodel import Session, select
+from emon_tools.fastapi.core.deps import CurrentUser
+from emon_tools.fastapi.models.db import DataPath
+
+
+class DataPathController:
+    """DataPath Controller"""
+    @staticmethod
+    def get_data_path(
+        *,
+        session: Session,
+        current_user: CurrentUser,
+        item_id: int
+    ) -> DataPath | None:
+        """
+        Retrieve a data path from the database by their id.
+
+        Args:
+            session (Session): The database session to use for the query.
+            email (str): The email address of the user to retrieve.
+
+        Returns:
+            User | None: The user object if found, otherwise None.
+        """
+        statement = select(DataPath).where(DataPath.id == item_id)
+        if not current_user.is_superuser:
+            statement.where(DataPath.owner_id == current_user.id)
+        result = session.exec(statement).first()
+        return result
+
+    @staticmethod
+    def count_data_path(
+        *,
+        session: Session
+    ) -> int:
+        """
+        Count DataPaths present.
+
+        Args:
+            session (Session): The database session to use for the query.
+
+        Returns:
+            int: Number of DataPaths in data base
+        """
+        statement = select(DataPath)
+        result = session.exec(statement).all()
+        return len(result)
