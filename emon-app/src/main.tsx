d5a7abab101a031519d8992b7060ded77a3c4fa9
mano8 <eli.serra173@gmail.com>
2025-03-18 03:42:40 +0100

    feat: add AuthProvider and BrowserRouter to main application for improved context management and routing

--- emon-app/src/main.tsx
+++ emon-app/src/main.tsx
@@ -6,6 +6,8 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query';
 import App from './App';
+import AuthProvider from './contextes/auth.context';
+import { BrowserRouter } from 'react-router';
 
 // Create a client
 const queryClient = new QueryClient();
@@ -13,7 +15,11 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
     </QueryClientProvider>   
   </StrictMode>
 )
