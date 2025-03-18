34c4baa12db4d7d1a54d4b23df0f27d50cbb010a
mano8 <eli.serra173@gmail.com>
2025-03-18 03:42:28 +0100

    refactor: simplify Layout component structure by removing AuthProvider and BrowserRouter

--- emon-app/src/layout.tsx
+++ emon-app/src/layout.tsx
@@ -1,39 +1,37 @@
-import { AppSidebar } from "@/routes/app-sidebar"
-import { Separator } from "@/components/ui/separator"
-import { BrowserRouter } from "react-router";
+import { AppSidebar } from "@/routes/app-sidebar";
+import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from "@/components/ui/sidebar"
-import { Toaster } from "@/components/ui/toaster"
+} from "@/components/ui/sidebar";
+import { Toaster } from "@/components/ui/toaster";
 import { Breadcrumbs } from "@/routes/breadcrumbs";
-import AuthProvider from "./providers/auth-provider";
+import { ReactNode } from "react";
 
+type LayoutProps = {
+  children: ReactNode;
+}
 
-export default function Layout(
-  { children }: { children: React.ReactNode }
-) {
+export default function Layout({
+  children
+}: LayoutProps) {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <SidebarProvider>
-          <AppSidebar />
-          <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-              <div className="flex items-center gap-2 px-4">
-                <SidebarTrigger className="-ml-1" />
-                <Separator orientation="vertical" className="mr-2 h-4" />
-                <Breadcrumbs />
-              </div>
-            </header>
-            <div className="min-h-[100vh] md:min-h-min px-4">
-              {children}
-            </div>
-            <Toaster />
-          </SidebarInset>
-        </SidebarProvider>
-      </BrowserRouter>
-    </AuthProvider>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumbs />
+          </div>
+        </header>
+        <div className="min-h-[100vh] md:min-h-min px-4">
+          {children}
+        </div>
+        <Toaster />
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
