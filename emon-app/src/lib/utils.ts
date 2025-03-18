d159167f81260114fcc4b0a167acd86a9a28bc1b
mano8 <eli.serra173@gmail.com>
2025-03-18 03:39:49 +0100

    feat: add utility functions for image path, API URL retrieval, and ID validation

--- emon-app/src/lib/utils.ts
+++ emon-app/src/lib/utils.ts
@@ -1,6 +1,23 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { idSchemeIn } from "./comon-schemas"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const getImagePath = (imgName?: string) => {
+  const imgPath = import.meta.env.VITE_IMAGES_URL
+  return imgPath && imgName ? `${imgPath}/${imgName}` : undefined;
+}
+
+export const getApiUrl = (url?: string) => {
+  const baseUrl = import.meta.env.VITE_API_URL
+  return baseUrl && url ? `${baseUrl}${url}` : '';
+}
+
+export const validateId = (item_id?: string) => {
+  const validate = idSchemeIn.safeParse(item_id);
+  if(validate.success === true)
+  return validate.success === true ? Number(item_id) : undefined;
 }
