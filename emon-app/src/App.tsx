085e6092641c80636c7611587ce4f39301975b65
mano8 <eli.serra173@gmail.com>
2025-03-18 03:42:21 +0100

    feat: implement dynamic routing based on authentication status for improved navigation

--- emon-app/src/App.tsx
+++ emon-app/src/App.tsx
@@ -1,43 +1,49 @@
 import Layout from '@/layout';
 import { Route, Routes } from 'react-router';
-import { routes } from './routes/routes';
+import { publicRoutes, privateRoutes, RouteListType } from './routes/routes';
 import { createElement } from 'react';
-
+import { useAuth } from './hooks/use-auth';
 
 function App() {
+  const { isAuthenticated } = useAuth();
+  const setRoutes = (routes: RouteListType[]) => {
+    return routes.map((route, i) => (
+      route.routes ? (
+        <Route
+          key={i}
+          path={route.path}
+        >
+          <Route
+            index
+            element={createElement(route.element)}
+          />
+          {route.routes.map((subroute, j) => (
+            <Route
+              key={`${i}_${j}`}
+              path={subroute.path}
+              element={createElement(subroute.element as React.ComponentType<unknown>)}
+            />
+          ))}
+        </Route>
+      ) : (
+        <Route
+          key={i}
+          path={route.path}
+          element={createElement(route.element as React.ComponentType<unknown>)}
+        />
+      )
+    ))
+  }
 
-  
   return (
     <Layout>
-      <Routes>
-        {routes.map((route, i) => (
-          route.routes ? (
-            <Route
-              key={i}
-              path={route.path}
-            >
-              <Route
-                  index
-                  element={createElement(route.element)}
-                />
-              {route.routes.map((subroute, j) => (
-                <Route
-                  key={`${i}_${j}`}
-                  path={subroute.path}
-                  element={createElement(subroute.element)}
-                />
-              ))}
-            </Route>
-          ) : (
-            <Route
-              key={i}
-              path={route.path}
-              element={createElement(route.element)}
-            />
-          )
-        ))}
-      </Routes>
-    </Layout>
+          <Routes>
+            {[
+              setRoutes(publicRoutes),
+              isAuthenticated ? setRoutes(privateRoutes) : null
+            ]}
+          </Routes>
+        </Layout>
   )
 }
 
