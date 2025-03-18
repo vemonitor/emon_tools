b67b457b96ae2b78ca0a2bd990a8c88d009d3fe7
mano8 <eli.serra173@gmail.com>
2025-03-18 10:16:05 +0100

    feat: enhance ID validation functions by adding support for output validation and new utility methods

--- emon-app/src/lib/utils.ts
+++ emon-app/src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { idSchemeIn } from "./comon-schemas"
+import { idSchemeIn, idSchemeOut } from "./comon-schemas"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -16,8 +16,23 @@ export const getApiUrl = (url?: string) => {
   return baseUrl && url ? `${baseUrl}${url}` : '';
 }
 
+export const validateStringId = (item_id?: string) => {
+  const validateIn = idSchemeIn.safeParse(item_id);
+  const validateOut = idSchemeOut.safeParse(Number(item_id));
+  return validateIn.success === true && validateOut.success === true ? Number(item_id) : 0;
+}
+
 export const validateId = (item_id?: string) => {
-  const validate = idSchemeIn.safeParse(item_id);
-  if(validate.success === true)
-  return validate.success === true ? Number(item_id) : undefined;
+  const validateIn = idSchemeIn.safeParse(item_id);
+  const validateOut = idSchemeOut.safeParse(Number(item_id));
+  return validateIn.success === true && validateOut.success === true ? Number(item_id) : 0;
+}
+
+export const validateIds = (route_id?: string, props_id?: number): number => {
+  const validateRoute = validateId(route_id);
+  const validateProps = idSchemeOut.safeParse(props_id);
+  if(validateProps.success === true){
+    return props_id ? props_id : 0
+  }
+  return validateRoute > 0 ? validateRoute : 0;
 }
