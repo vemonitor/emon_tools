374f4dca87d04b5ffd6d909c031fd16dec1ab112
mano8 <eli.serra173@gmail.com>
2025-03-21 03:23:51 +0100

    refactor: enhance accessibility in LoginDialog with visually hidden text

--- emon-app/src/components/nav_sidebar/login-dialog.tsx
+++ emon-app/src/components/nav_sidebar/login-dialog.tsx
@@ -1,28 +1,31 @@
 import { Button } from "@/components/ui/button"
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import {
   Dialog,
   DialogContent,
+  DialogTrigger,
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog"
 import { LoginForm } from "@/routes/user/login-form"
- 
+
 export function LoginDialog() {
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="outline">Login</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-      <DialogHeader>
-        <DialogTitle>Edit profile</DialogTitle>
-        <DialogDescription>
-          Make changes to your profile here. Click save when you're done.
-        </DialogDescription>
-      </DialogHeader>
-        <LoginForm />
+      <DialogContent className="sm:max-w-[425px] border-none">
+        <DialogHeader>
+          <DialogTitle><VisuallyHidden>Login Form Dialog</VisuallyHidden></DialogTitle>
+          <DialogDescription>
+            <VisuallyHidden>Enter credentials to login.</VisuallyHidden>
+          </DialogDescription>
+        </DialogHeader>
+        <div className="p-3">
+          <LoginForm />
+        </div>
       </DialogContent>
     </Dialog>
   )
