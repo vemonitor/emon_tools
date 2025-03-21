c7c06541adbfce8554daddfc8bb9bdb610edae52
mano8 <eli.serra173@gmail.com>
2025-03-21 04:18:25 +0100

    refactor: simplify ListEmonHost component by removing authentication check and unnecessary imports

--- emon-app/src/routes/emon-host/list.tsx
+++ emon-app/src/routes/emon-host/list.tsx
@@ -1,25 +1,18 @@
 import { columns, EmonHost } from "./table-header-columns"
 import { useAuth } from "@/hooks/use-auth";
-import { useNavigate } from "react-router";
-import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import ListView from "@/components/layout/list-item-view";
 
 function ListEmonHost() {
-  const { isAuthenticated, fetchWithAuth } = useAuth();
-  const navigate = useNavigate();
-  useEffect(() => {
-      if (!isAuthenticated) {
-        navigate("/login");
-      }
-    }, [isAuthenticated, navigate]);
+  const { fetchWithAuth } = useAuth();
+  
   const items = useQuery(
     {
       queryKey: ['emon_hosts'],
       retry: false,
       queryFn: () =>
         fetchWithAuth(
-          `http://127.0.0.1:8000/api/v1/emon_host/`,
+          `/api/v1/emon_host/`,
           {
             method: 'GET',
           }
