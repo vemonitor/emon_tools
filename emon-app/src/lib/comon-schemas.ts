ac442faf779fc6c25f1848ddfc5246da643dea2d
mano8 <eli.serra173@gmail.com>
2025-03-21 03:24:22 +0100

    feat: add new string validation schemas for alphanumeric, email, and name attributes

--- emon-app/src/lib/comon-schemas.ts
+++ emon-app/src/lib/comon-schemas.ts
@@ -9,3 +9,49 @@ export const idSchemeIn = z.string()
 
 export const idSchemeOut = z.number()
   .positive();
+
+export const AlphaNumString = z.string()
+  .min(1)
+  .max(50)
+  .regex(/^[A-Za-z0-9]+$/, {
+    message: 'Please enter a valid attribute (Only Alphabetical characters with accents and spaces are accepted)',
+  })
+
+export const KeyString = z.string()
+  .min(1)
+  .max(50)
+  .regex(/^[A-Za-z0-9_-]+$/, {
+    message: 'Please enter a valid attribute (Only Alphabetical characters with accents and spaces are accepted)',
+  })
+
+export const MailString = z.string()
+  .min(1)
+  .max(70)
+  .email()
+  .regex(/^[A-Za-z0-9_@.-]+$/, {
+    message: 'Please enter a valid attribute (Only Alphabetical characters with accents and spaces are accepted)',
+  })
+
+export const NameString = z.string()
+  .min(1)
+  .max(50)
+  .regex(/^[A-Za-z0-9_ -]+$/, {
+    message: 'Please enter a valid attribute (Only Alphabetical characters with accents and spaces are accepted)',
+  })
+
+export const TextString = z.string()
+  .min(1)
+  .max(50)
+  .regex(/^[A-Za-z0-9À-ÖØ-öø-ÿ_ -]+$/, {
+    message: 'Please enter a valid attribute (Only Alphabetical characters with accents and spaces are accepted)',
+  })
+
+export const nullableName = z.union([
+  z.null(),
+  z.string()
+    .min(1)
+    .max(50)
+    .regex(/^[A-Za-zÀ-ÖØ-öø-ÿ ]+$/, {
+      message: 'Please enter a valid attribute (Only Alphabetical characters with accents and spaces are accepted)',
+    })
+  ])
