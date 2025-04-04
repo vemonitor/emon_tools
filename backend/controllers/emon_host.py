ca72b2d4a1189e6c3671fbb26b457d183b60a88f
mano8 <eli.serra173@gmail.com>
2025-04-04 05:02:36 +0200

    feat: add method to retrieve EmonHost by slug with user permissions

--- backend/controllers/emon_host.py
+++ backend/controllers/emon_host.py
@@ -3,6 +3,7 @@ EmonHost Controller
 """
 import uuid
 from sqlmodel import Session, select
+from backend.core.deps import CurrentUser
 from backend.models.db import EmonHost
 from backend.models.db import EmonHostCreate
 
@@ -33,6 +34,28 @@ class EmonHostController:
         session.commit()
         session.refresh(db_item)
         return db_item
+
+    @staticmethod
+    def get_emon_host_by_slug(
+        *,
+        session: Session,
+        current_user: CurrentUser,
+        slug: str
+    ) -> EmonHost | None:
+        """
+        Count DataPaths present.
+
+        Args:
+            session (Session): The database session to use for the query.
+
+        Returns:
+            int: Number of DataPaths in data base
+        """
+        statement = select(EmonHost).where(EmonHost.slug == slug)
+        if not current_user.is_superuser:
+            statement.where(EmonHost.owner_id == current_user.id)
+        result = session.exec(statement).first()
+        return result
 
     @staticmethod
     def count_emon_hosts(
