428d9183f17091a1c6fc38624b3e0d9af304a169
mano8 <eli.serra173@gmail.com>
2025-04-04 04:52:53 +0200

    feat: enhance graphTypes with new data structures for feed and file management

--- emon-app/src/lib/graphTypes.ts
+++ emon-app/src/lib/graphTypes.ts
@@ -1,10 +1,7 @@
 
   /**
-   * Represents a single data point for the graph.
-   * Requires a numeric `date` property and allows additional key-value pairs.
+   * Represents fina data and feed meta data type.
    */
-  export type SelectedFeedsProps = {file_name: string, position: GraphLocationProps}
-
   export type FeedMetaOut = {
     start_time: number,
     end_time: number,
@@ -13,6 +10,10 @@
     size: number,
   }
 
+  /**
+   * Represents fina data and feed db item type.
+   * From Emon-tools fastapi db.
+   */
   export type FileDbOut = {
     file_id: number,
     name: string,
@@ -21,18 +22,7 @@
     emonhost_id: number,
   }
 
-  export type FileItemProps = {
-    file_name: string,
-    name: string,
-    meta: FeedMetaOut,
-    file_db?: FileDbOut,  
-  };
-
-  export type FeedMetaResponse = {
-    success: boolean,
-    feed_id: number,
-    data: FeedMetaOut[]
-  }
+  export type RequestSource = "emoncms" | "files"
 
   export type DataViewerProps = {
     path_id: number
@@ -42,13 +32,68 @@
     date: number;
     [key: string]: number | null;
   }
-
-  export type FinaSourceProps = "emoncms" | "archive"
   
   /** Allowed locations for a graph feed */
   export type GraphLocationProps = "left" | "right";
 
+  export type GraphSource = "feeds" | "files";
+
+  export interface FeedDb {
+    id: number;
+    userid: number,
+    name: string,
+    tag: string;
+    public: boolean;
+    engine: string;
+    unit: string;
+    meta: FeedMetaOut,
+    files_db?: FileDbOut[]
+  }
+  
+  export interface FeedListDb {
+      success: boolean
+      host_id: number
+      nb_feeds: number
+      feeds: FeedDb[]    
+  }
+
+  export type FinaFileDb = {
+    file_name: string,
+    name: string,
+    meta: FeedMetaOut,
+    file_db: {
+      file_id: number,
+      name: string,
+      slug: string,
+      feed_id: number,
+      emonhost_id: number
+    }
+  };
+
+  export type FinaFilesListDb = {
+    success: boolean,
+    path_id: number,
+    nb_added: number,
+    files: FinaFileDb[]
+  };
+
+  export type PreSelectedToGraph = {
+    is_checked: boolean;
+    id: number;
+    side: GraphLocationProps;
+  }
+
+  export type SelectedToGraph = {
+    is_checked: boolean;
+    id: number;
+    side: GraphLocationProps;
+    name: string;
+    meta: FeedMetaOut;
+    file_db?: FileDbOut[];
+  }
+
   export type SelectedFileItem = {
+    type: "file";
     is_checked: boolean;
     file_name: string;
     side: GraphLocationProps;
@@ -57,8 +102,51 @@
     file_db?: FileDbOut;
   }
 
+  export type SelectedEmonItem = {
+    type: "feed";
+    is_checked: boolean;
+    id: number;
+    side: GraphLocationProps;
+    name: string;
+    meta: FeedMetaOut;
+    files_db?: FileDbOut[];
+  }
+
+  export type SelectedFeedItem = SelectedFileItem | SelectedEmonItem;
+
+  export type timeSerie = [number, number | null | undefined, number | null | undefined, number | null | undefined]
+
+  export type FinaDataIn = {
+    success: boolean,
+    file_id: number,
+    feed_id: number,
+    datapath_id: number,
+    emonhost_id: number,
+    file_name: string,
+    name: string,
+    data: timeSerie[],
+  }
+
+  export type FeedDataIn = {
+    success: boolean,
+    id: number,
+    name: string,
+    data: timeSerie[],
+  }
+
+  export type GraphDataIn = FinaDataIn | FeedDataIn
+
   /** Represents a feed with its id and the location it should be displayed on */
-  export type GraphFeedProps = { feed_id: number; location: GraphLocationProps };
+  export type GraphFeedProps = {
+    id: number;
+    name: string;
+    location: GraphLocationProps;
+    db_data?: {
+      datapath_id: number;
+      emonhost_id: number;
+      feed_id: number;
+    }[]
+  };
   
   /** Contains data points and feed configurations for a line chart */
   export interface LineChartDataProps {
@@ -74,6 +162,25 @@
     right_bottom: number[];
   };
   
+  export type NavigationMenu = {
+    can_reload: boolean,
+    can_go_start: boolean;
+    can_go_back: boolean;
+    can_go_after: boolean;
+    can_go_end: boolean;
+    can_zoom_in: boolean;
+    can_zoom_out: boolean;
+    zoom_level: number;
+    move_level: number;
+  }
+  
+  export type GraphSelector = {
+    left: string | number,
+    right: string | number,
+    refAreaLeft: number,
+    refAreaRight: number
+  }
+
   /** Basic zoom parameters for the graph */
   export type GraphZoom = {
     topLeft: string | number;
